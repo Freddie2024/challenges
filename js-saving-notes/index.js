08aad0c665d9c8797f03a12b34203069a7299c39
FRIDA LEMKE <frida.lemke@gmx.de>
2024-09-05 21:12:54 +0200

    add savings-calculator

---
+++ js-saving-notes/index.js
@@ -0,0 +1,6 @@
+const startingBalance = 1000
+let monthlyContribution = 50
+const months = 12
+let totalSavings = startingBalance + (monthlyContribution * months)
+let text = "After 12 months of saving, you will have $" + totalSavings + " in your account."
+console.log(text)
