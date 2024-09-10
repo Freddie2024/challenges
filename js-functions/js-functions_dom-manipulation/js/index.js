a146696a492159dcbf7bb8e5bf2cbfcd7ab5b806
FRIDA LEMKE <frida.lemke@gmx.de>
2024-09-10 11:02:54 +0200

    create variables and return statements

--- js-functions/js-functions_dom-manipulation/js/index.js
+++ js-functions/js-functions_dom-manipulation/js/index.js
@@ -5,10 +5,13 @@ const blueButton = document.querySelector("[data-js=js-blue-button]");
 const grayButton = document.querySelector("[data-js=js-gray-button]");
 
 /*
-We want to add some functionality to the buttons on this page. When pressing a button, the corresponding color should be applied to the box.
+We want to add some functionality to the buttons on this page. When pressing a button, 
+the corresponding color should be applied to the box.
   1: Create a function removeAllColors which removes all color classes from the box.
-  2: Create a function addColor which takes a class name as a parameter and adds the class to the box.
-  3: Use the two functions in the event listeners of the buttons to color in the box when the button is clicked.
+  2: Create a function addColor which takes a class name as a parameter and adds the 
+  class to the box.
+  3: Use the two functions in the event listeners of the buttons to color in the box 
+  when the button is clicked.
 */
 
 redButton.addEventListener("click", () => {
