38929d2e629e0b70388607d610e1943717ce89ea
FRIDA LEMKE <frida.lemke@gmx.de>
2024-09-11 21:44:40 +0200

    add createelement-challenges

--- js-forms/js-forms_checkbox-input/js/index.js
+++ js-forms/js-forms_checkbox-input/js/index.js
@@ -3,22 +3,34 @@ console.clear();
 const form = document.querySelector('[data-js="form"]');
 const tosError = document.querySelector('[data-js="tos-error"]');
 const tosCheckbox = document.querySelector('[data-js="tos"]');
+const successMessage = document.querySelector('[data-js="success"]');
 
 function hideTosError() {
-  tosError.setAttribute("hidden", "");
+  tosError.setAttribute("hidden", true);
 }
 
 function showTosError() {
   tosError.removeAttribute("hidden");
 }
 
+function showSuccess() {
+  successMessage.removeAttribute("hidden");
+}
+
+function hideSuccess() {
+  successMessage.setAttribute("hidden", true);
+
 form.addEventListener("submit", (event) => {
   event.preventDefault();
 
-  // --v-- write your code here --v--
-
-  // --^-- write your code here --^--
-
-  // eslint-disable-next-line no-alert
+const isChecked = tosCheckbox.checked;
+if (isChecked) {
+  hideTosError();
+  showSuccess();
+  form.reset();
   alert("Form submitted");
+  return;
+}
+hideSuccess();
+showTosError();  
 });
