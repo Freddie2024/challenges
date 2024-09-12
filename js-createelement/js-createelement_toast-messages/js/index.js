d9c30c4abcb94e27a08349a41ffe5269041d4cab
FRIDA LEMKE <frida.lemke@gmx.de>
2024-09-12 16:11:40 +0200

    add provided solution in comment

--- js-createelement/js-createelement_toast-messages/js/index.js
+++ js-createelement/js-createelement_toast-messages/js/index.js
@@ -4,15 +4,28 @@ const toastContainer = document.querySelector('[data-js="toast-container"]');
 const addButton = document.querySelector('[data-js="add-button"]');
 const clearButton = document.querySelector('[data-js="clear-button"]');
 
-addButton.addEventListener("click", (event) => {
+addButton.addEventListener("click", () => {
   // Exercise: Append a new entry to the toast messages container
+
+  /* copied from the provided solutions: 
+  const date = new Date().toDateString();
+  const message = `New message! Received at: ${date}`;
+
+  const toast = document.createElement("li");
+  toast.classList.add("toast-container__message");
+  toast.textContent = message;
+  toastContainer.append(toast);
+*/
+
+  
   const newElement = document.createElement("li");
   toastContainer.append(newElement);
   newElement.classList.add("toast-container__message");
   newElement.textContent = "I'm a toast message.";
+  
 });
 
-clearButton.addEventListener("click", (event) => {
+clearButton.addEventListener("click", () => {
   // Exercise: Clear the stack of toast messages
   toastContainer.innerHTML = "";
 });
