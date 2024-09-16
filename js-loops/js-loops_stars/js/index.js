62606aae649a20f1d8a8419c2fe26ae3ec8c0fa2
FRIDA LEMKE <frida.lemke@gmx.de>
2024-09-16 20:08:08 +0200

    add five empty stars with for-loop

--- js-loops/js-loops_stars/js/index.js
+++ js-loops/js-loops_stars/js/index.js
@@ -8,6 +8,13 @@ function renderStars() {
 
   //--v-- your code here --v--
 
+  for (let stars = 1; stars <= 5; stars++) {
+    const emptyStar = document.createElement("img");
+    emptyStar.src = "assets/star-empty.svg"
+    starContainer.append(emptyStar);
+  }
+
+
   //--^-- your code here --^--
 }
 
