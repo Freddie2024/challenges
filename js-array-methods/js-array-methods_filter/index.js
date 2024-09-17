d3a64fffb87fcd9a1242030bd7c01caa5068ddaf
FRIDA LEMKE <frida.lemke@gmx.de>
2024-09-17 17:15:18 +0200

    filter cards with id=2

--- js-array-methods/js-array-methods_filter/index.js
+++ js-array-methods/js-array-methods_filter/index.js
@@ -23,13 +23,26 @@ const cards = [
   },
 ];
 
-const onlyCardWithIdTwo = null;
+const onlyCardWithIdTwo = cards.filter((card) => {
+  return card.id === "2";
+});
+console.log(onlyCardWithIdTwo)
 
-const allCardsWith3Tags = null;
+/*
+const allCardsWith3Tags = cards.filter((card) => {
+  return card.
+}
+);
 
-const allCardsThatAreNotBookmarked = null;
+const allCardsThatAreNotBookmarked = cards.filter((card) => {
+  return card.
+}
+);
 
-const allCardsWithTagsHTMLOrJSThatAreBookmarked = null;
+const allCardsWithTagsHTMLOrJSThatAreBookmarked = cards.filter((card) => {
+  return card.
+}
+);
 
 export {
   onlyCardWithIdTwo,
@@ -37,3 +50,4 @@ export {
   allCardsThatAreNotBookmarked,
   allCardsWithTagsHTMLOrJSThatAreBookmarked,
 };
+*/
