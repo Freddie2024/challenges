b659025b79d6de995396dbd4a9c791f7715b77cb
FRIDA LEMKE <frida.lemke@gmx.de>
2024-09-18 22:21:31 +0200

    add maybe final solution

--- js-structure/js-structure_spacerocket/js/launchSequence.js
+++ js-structure/js-structure_spacerocket/js/launchSequence.js
@@ -1,6 +1,7 @@
 // Implement the launch sequence function here and export it as the default export.
 import { loadPayload } from "./core/load.js";
-import { NFSAT, FISHSAT } from "./payload/satellites.js";
+import { NFSAT } from "./payload/satellites.js";
+import { FISHSAT } from "./payload/satellites.js";
 import { fuel } from "./core/fuel.js";
 import { countdown } from "./core/countdown.js";
 import { liftoff } from "./core/liftoff.js";
@@ -9,8 +10,8 @@ import { getNewRocket, getRocket } from "./core/rocket.js";
 
 export default function launch() {
     window.rocket = getNewRocket();
-    const payload = [NFSAT, FISHSAT];
-    loadPayload(payload);
+    loadPayload(NFSAT);
+    loadPayload(FISHSAT);
     fuel();
     const rocket = getRocket();
     rocket.fuel = true;  
