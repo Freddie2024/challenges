8d0ce14880ea68b5dd944e2784b59b5701b296f7
FRIDA LEMKE <frida.lemke@gmx.de>
2024-09-19 10:54:55 +0200

    simplify code

--- js-structure/js-structure_spacerocket/js/launchSequence.js
+++ js-structure/js-structure_spacerocket/js/launchSequence.js
@@ -6,27 +6,14 @@ import { fuel } from "./core/fuel.js";
 import { countdown } from "./core/countdown.js";
 import { liftoff } from "./core/liftoff.js";
 import { deployPayload } from "./core/deploy.js";
-import { getNewRocket, getRocket } from "./core/rocket.js";
 
 export default function launch() {
-    window.rocket = getNewRocket();
-    loadPayload(NFSAT);
-    loadPayload(FISHSAT);
-    fuel();
-    const rocket = getRocket();
-    rocket.fuel = true;  
-    const countdownLength = rocket.requiredCountdown;
-    for (let i = 0; i < countdownLength; i++) {
-        countdown();
-        console.log(`Countdown step ${i + 1}, rocket countdown: ${rocket.countdown}`);
-    }
-        if (rocket.countdown === rocket.requiredCountdown) {
-            liftoff();
-            rocket.liftoff = true;
-        } else {
-            throw new Error("Cannot lift off without the correct countdown sequence.")
-        }
-    
-    deployPayload();
-    rocket.payloadDeployed = true;
-    }
+   loadPayload(FISHSAT);
+   loadPayload(NFSAT);
+   fuel();
+   for (let i=1; i<=5; i++) {
+    countdown() 
+   };
+   liftoff();
+   deployPayload();
+}
