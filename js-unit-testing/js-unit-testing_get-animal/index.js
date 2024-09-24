d273cb45c53684f931264fdf7ea1224aac2d5b63
FRIDA LEMKE <frida.lemke@gmx.de>
2024-09-24 11:17:45 +0200

    add solution

--- js-unit-testing/js-unit-testing_get-animal/index.js
+++ js-unit-testing/js-unit-testing_get-animal/index.js
@@ -1,1 +1,10 @@
-export function getAnimal() {}
+export function getAnimal(animal) {
+    if (animal === "cats") {
+        return "I totally love cats!";
+    } else if (!animal) { 
+return "I do not like animals at all!"
+    } else {
+    const animalPluralName = animal.endsWith('s') ? animal : animal + 's';
+    return `I like ${animalPluralName}!`;
+    } 
+}
