629bf327ad96ea52d8d92638dc59a32560544f07
FRIDA LEMKE <frida.lemke@gmx.de>
2024-10-01 11:22:59 +0200

    add solution

--- react-basics/react-basics_hello-world-article/src/App.jsx
+++ react-basics/react-basics_hello-world-article/src/App.jsx
@@ -1,5 +1,12 @@
 import "./App.css";
 
 export default function App() {
-  return <div>Replace me with your Component!</div>;
+  return <HelloWorldArticle/>;
+}
+
+export function HelloWorldArticle () {
+  return <article>
+    <h1>Hi, hello!</h1>
+    <p>How are you today, beautiful people?</p>
+  </article>
 }
