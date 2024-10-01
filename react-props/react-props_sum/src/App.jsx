c4bc8b9e0de18abd217ba3f267d3aba01fedb43e
FRIDA LEMKE <frida.lemke@gmx.de>
2024-10-01 15:57:16 +0200

    add solution

--- react-props/react-props_sum/src/App.jsx
+++ react-props/react-props_sum/src/App.jsx
@@ -1,5 +1,13 @@
 import "./App.css";
 
+export function Sum({valueA, valueB}) {
+  return (
+    <p>
+      {valueA} + {valueB} = {valueA + valueB}
+    </p>
+  );
+}
+
 export default function App() {
-  return <h1>Replace me with your component!</h1>;
+  return <Sum valueA={1} valueB={2}/>;
 }
