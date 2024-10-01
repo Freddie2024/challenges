da116a822f0b6ae7f75c83efb3c629d916b9fc8e
FRIDA LEMKE <frida.lemke@gmx.de>
2024-10-01 12:05:29 +0200

    add solution

--- react-basics/react-basics_attributes/src/App.jsx
+++ react-basics/react-basics_attributes/src/App.jsx
@@ -1,5 +1,14 @@
 import "./App.css";
 
 export default function App() {
-  return <div>Replace me with an article</div>;
+  return <Article/>;
+}
+
+export function Article () {
+  return <article className="article">
+  <h2 className="article__title">Text of my Choice</h2>
+  <label htmlFor="input__field">Some input</label>
+  <input id="input__field"></input>
+  <a className="article__link" href="https://en.wikipedia.org/wiki/React_(JavaScript_library)"></a>
+</article>
 }
