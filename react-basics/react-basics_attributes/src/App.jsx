bcb765854d645e4745dda2be36be7133d1956cc9
FRIDA LEMKE <frida.lemke@gmx.de>
2024-10-02 14:02:15 +0200

    add text to link

--- react-basics/react-basics_attributes/src/App.jsx
+++ react-basics/react-basics_attributes/src/App.jsx
@@ -9,6 +9,6 @@ export function Article () {
   <h2 className="article__title">Text of my Choice</h2>
   <label htmlFor="input__field">Some input</label>
   <input id="input__field"></input>
-  <a className="article__link" href="https://en.wikipedia.org/wiki/React_(JavaScript_library)"></a>
+  <a className="article__link" href="https://en.wikipedia.org/wiki/React_(JavaScript_library)">Further reading</a>
 </article>
 }
