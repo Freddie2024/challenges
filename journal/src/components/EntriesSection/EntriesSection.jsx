df4b49e900e25889b849271d394234dc68cf6edf
FRIDA LEMKE <frida.lemke@gmx.de>
2024-10-03 19:19:09 +0200

    create base structure

---
+++ journal/src/components/EntriesSection/EntriesSection.jsx
@@ -0,0 +1,14 @@
+import './EntriesSection.css';
+
+export default function EntriesSection() {
+    return (
+        <section className=''>
+           <tabbar>
+                <tab> badge </tab>
+                <tab> badge </tab>
+            <ul>
+                <Entry>Icon-Button</Entry>
+            </ul>
+        </section>
+    );
+}
---
+++ journal/src/components/EntryForm/EntryForm.jsx
@@ -0,0 +1,13 @@
+import './EntryForm.css';
+
+export default function EntryForm() {
+    return (
+        <form className='form'>
+            <input></input>
+            <textarea></textarea>
+            <input></input>
+            <textarea></textarea>
+            <button>CREATE</button>
+        </form>
+    );
+}
