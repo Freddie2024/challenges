5492ed3f73c25a40db272f93bbfde1872845483b
FRIDA LEMKE <frida.lemke@gmx.de>
2024-10-04 16:48:30 +0200

    add state variable and toggle

--- react-state/react-state_journal-app-favorite-button/src/components/FavoriteButton/index.jsx
+++ react-state/react-state_journal-app-favorite-button/src/components/FavoriteButton/index.jsx
@@ -1,20 +1,28 @@
 import "./FavoriteButton.css";
-import StarFilled from "./star-filled.svg?react";
-import Star from "./star.svg?react";
+import { useState } from "react";
 
 export default function FavoriteButton() {
   // this should be a state variable
-  const isFavorite = false;
+  const [isFavorite, setIsFavorite] = useState(false);
 
   return (
     <button
       className="favorite-button"
       onClick={() => {
+        setIsFavorite(!isFavorite)
         console.log("favorite button clicked");
       }}
       aria-label="favorite"
     >
-      {isFavorite ? <StarFilled /> : <Star />}
+      {isFavorite ? (
+        <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path d="M12 2L15.09 8.26L22 9.27L17 14.14L18.18 21.02L12 17.77L5.82 21.02L7 14.14L2 9.27L8.91 8.26L12 2Z" fill="#8236ff" stroke="#3f0097" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+      </svg>
+      ) : (
+        <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path d="M12 2L15.09 8.26L22 9.27L17 14.14L18.18 21.02L12 17.77L5.82 21.02L7 14.14L2 9.27L8.91 8.26L12 2Z" stroke="#252629" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+      </svg>
+      )}
     </button>
   );
 }
