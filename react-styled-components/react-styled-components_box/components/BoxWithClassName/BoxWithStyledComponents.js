585710ade29bf9949bb2123d1195781977ca1c28
FRIDA LEMKE <frida.lemke@gmx.de>
2024-10-15 12:22:10 +0200

    add solution

---
+++ react-styled-components/react-styled-components_box/components/BoxWithClassName/BoxWithStyledComponents.js
@@ -0,0 +1,24 @@
+import styled, { css } from "styled-components";
+
+const StyledBox = styled.div`
+  width: 100px;
+  height: 100px;
+  background-color: green;
+  margin: 2rem;
+
+  ${({ $isBlack }) =>
+    $isBlack && 
+    css`background-color: black;
+  `}
+
+  &:hover {
+    background-color: red;
+  }
+
+`;
+
+export default function BoxWithStyledComponents({$isBlack}) {
+    return <StyledBox $isBlack={$isBlack} />
+}
+
+
