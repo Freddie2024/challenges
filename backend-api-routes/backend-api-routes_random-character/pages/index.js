8c5830a68105a679ea2e2f1ed6cbf741acf1636f
FRIDA LEMKE <frida.lemke@gmx.de>
2024-10-25 11:57:01 +0200

    add solution

--- backend-api-routes/backend-api-routes_random-character/pages/index.js
+++ backend-api-routes/backend-api-routes_random-character/pages/index.js
@@ -1,3 +1,29 @@
+import useSWR from "swr";
+
 export default function HomePage() {
-  return <h1>Hello from Next.js!</h1>;
+  
+  const fetcher = (url) => fetch(url).then((response) => response.json());
+  const { data, error, isValidating } = useSWR(`/api/random-character`, fetcher);
+
+  if (isValidating) {
+    return <h1>Loading...</h1>;
+  }
+
+  if (error) {
+    return <h1>Error loading data</h1>;
+  }
+
+  if (!data) {
+    return null;
+  }
+
+  return (
+    <>
+      <h1>{data.firstName} {data.lastName}</h1>
+      <small>
+        Twitter: {data.twitterName}; 
+        GeoHash: {data.geohashName}
+      </small>
+    </>
+  );
 }
